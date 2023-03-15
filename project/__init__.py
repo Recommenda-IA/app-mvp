5a444577acbeab07e49a5eeb96ce9cc1c8946e8f
Carlos Alberto <carlosalbertospace@hotmail.com>
2023-03-14 22:28:05 -0300

    Add: suporte Postgres

--- project/__init__.py
+++ project/__init__.py
@@ -2,22 +2,25 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
+import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask.logging import default_handler
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
