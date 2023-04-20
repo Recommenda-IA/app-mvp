5cf301d5b630bab12e7bbf5f25b9523d10625f04
Carlos Alberto Silva Junior <casjunior93@gmail.com>
2023-04-20 00:03:29 -0300

    Ajustes models e formulpario de banco de dados

--- project/main.py
+++ project/main.py
@@ -1,7 +1,10 @@
 # main.py
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
+from .models.models import Database_access
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 main = Blueprint('main', __name__)
 
@@ -36,4 +39,22 @@ def profile():
 @main.route('/database')
 @login_required
 def database():
+    return render_template('database.html', name=current_user.name)
+
+
+@main.route('/database/save-database', methods=['POST'])
+@login_required
+def database_save():
+    db_user = request.form.get('db_user')
+    db_password = request.form.get('db_password')
+    db_host = request.form.get('db_host')
+    db_name = request.form.get('db_name')
+
+    new_database = Database_access(db_user=db_user, db_host=db_host,
+                                   db_password=generate_password_hash(db_password, method='sha256'), db_name=db_name)
+    db.session.add(new_database)
+    db.session.commit()
+
+    flash('Banco de dados cadastrado')
+
     return render_template('database.html', name=current_user.name)
--- project/models/models.py
+++ project/models/models.py
@@ -2,11 +2,29 @@
 
 from flask_login import UserMixin
 from .. import db
+from datetime import datetime
+import pytz
+
+UTC = pytz.utc
+ASP = pytz.timezone('america/sao_paulo')
+datetime_ist = datetime.now(ASP)
 
 
 class User(UserMixin, db.Model):
     # primary keys are required by SQLAlchemy
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(1000), nullable=False)
+
+
+class Database_access(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    db_user = db.Column(db.String(100), nullable=False)
+    db_password = db.Column(db.String(100), nullable=False)
+    db_name = db.Column(db.String(1000), nullable=False)
+    db_host = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(
+        db.DateTime, default=datetime_ist, nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime_ist,
+                           onupdate=datetime_ist, nullable=False)
